d4b1e857c0d782d73b8cd5ca2d10a998faa1c36f
Alejandro <alejandro.castillo@payretailers.com>
2026-03-03 16:43:47 +0100

    Refactor: Add Currency enum, MoneyDto, and pass ClientId across layers

---
+++ Shared.Domain/Enums/Currency.cs
@@ -0,0 +1,18 @@
+namespace Shared.Domain.Enums;
+
+public enum Currency
+{
+    USD,
+    EUR,
+    GBP,
+    JPY,
+    MXN,
+    CAD,
+    CHF,
+    AUD,
+    NZD,
+    BRL,
+    ARS,
+    CLP,
+    COP
+}
