2f7ac29be6aecb9f7f77b8fcd5b15d678792e1df
Alejandro <alejandro.castillo@payretailers.com>
2026-03-05 18:55:49 +0100

    Add ShopDetails to MerchantDetails entity

--- Shared.Domain/Entities/MerchantDetails.cs
+++ Shared.Domain/Entities/MerchantDetails.cs
@@ -4,12 +4,28 @@ public record MerchantDetails
 {
     public string? MerchantId { get; init; }
     public string? MerchantName { get; init; }
+    public ShopDetails? Shop { get; init; }
 
     public MerchantDetails() { }
 
-    public MerchantDetails(string? merchantId, string? merchantName)
+    public MerchantDetails(string? merchantId, string? merchantName, ShopDetails? shop = null)
     {
         MerchantId = merchantId;
         MerchantName = merchantName;
+        Shop = shop;
+    }
+}
+
+public record ShopDetails
+{
+    public string? ShopId { get; init; }
+    public string? ShopName { get; init; }
+
+    public ShopDetails() { }
+
+    public ShopDetails(string? shopId, string? shopName)
+    {
+        ShopId = shopId;
+        ShopName = shopName;
     }
 }
